a63cf84be276b9f0dec9b56f6da21993751cf8d4
dylan <didiloy2001@gmail.com>
2024-03-18 18:38:20 +0100

    can now select a store by clicking on the item in the list and not only the checkbox

--- app/src/main/java/com/github/didiloy/promohub/selectstore/StoreAdapter.java
+++ app/src/main/java/com/github/didiloy/promohub/selectstore/StoreAdapter.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -37,12 +38,26 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(@NonNull StoreAdapter.ViewHolder holder, int position) {
+        //textview
         holder.textview_store_name.setText(stores[position].storeName);
+
+        //checkbox
         holder.checkBox.setOnCheckedChangeListener(null);
         holder.checkBox.setChecked(stores[position].isChecked);
         holder.checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> stores[position].isChecked = isChecked);
+
+        //image
         String imageUrl = CheapShark.IMG_BASE_URL + stores[position].images.get("logo");
-        Glide.with(holder.itemView.getContext()).load(imageUrl).placeholder(R.drawable.image_not_found).error(R.drawable.image_not_found).into(holder.imageView_logo);
+        Glide.with(holder.itemView.getContext()).load(imageUrl)
+                .placeholder(R.drawable.image_not_found)
+                .error(R.drawable.image_not_found)
+                .into(holder.imageView_logo);
+
+        //cardview
+        holder.cardView.setOnClickListener(v -> {
+            holder.checkBox.setChecked(!holder.checkBox.isChecked());
+            stores[position].isChecked = holder.checkBox.isChecked();
+        });
     }
 
     @Override
@@ -55,12 +70,14 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> 
         ImageView imageView_logo;
         TextView textview_store_name;
         CheckBox checkBox;
+        CardView cardView;
 
         public ViewHolder(@NonNull android.view.View itemView) {
             super(itemView);
             imageView_logo = itemView.findViewById(R.id.imageView_logo);
             textview_store_name = itemView.findViewById(R.id.textview_store_name);
             checkBox = itemView.findViewById(R.id.checkBox);
+            cardView = itemView.findViewById(R.id.cardView_store_select_item);
         }
     }
 }
